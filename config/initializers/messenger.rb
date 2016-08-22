99c478ef6a2f6807b81676b2106501475f84dcc7
Florian Barbato <flobarbato@gmail.com>
2016-08-22 14:54:48 +0200

    created messenger.rb

---
+++ config/initializers/messenger.rb
@@ -0,0 +1,4 @@
+Messenger.configure do |config|
+  config.verify_token      = '<VERIFY_TOKEN>' #will be used in webhook verifiction
+  config.page_access_token = 'EAAY3urykNT0BAIEY9YZAGK9KtkBd2JiELQH6c4pAfn2Cf3gO7lVLJLXs5qfUTxBPPaZB1razLIYnrXVd2NVCYvIGMKR7y5ED6KGAZByxZAQJbXqRHzzAzx5HSRVdrhI4dQ6FMd4oenbWAhHa4ZC9PjyauefRIm6DYdwtmqhWWZBwZDZD'
+end
