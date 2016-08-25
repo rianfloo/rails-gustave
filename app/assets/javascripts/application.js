2f0762852590d36fed157889e7692885bb3e2c41
Florian Barbato <flobarbato@gmail.com>
2016-08-26 00:11:40 +0200

    landing is ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,17 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+function openMessenger() {
+  window.open('https://www.messenger.com/t/gustavesommelier', 'messenger');
+  document.body.classList.add('inactive');
+  setTimeout(function(){
+    checkFocus = setInterval(function(){
+      if (document.hasFocus()) {
+        document.body.classList.remove('inactive');
+        clearInterval(checkFocus);
+      }
+    }, 1)
+  }, 1000);
+}
