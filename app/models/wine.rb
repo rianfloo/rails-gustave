41520503ad542f81c98249499cfdffc0bcc018bf
JFachetti <goillot.jeremy@gmail.com>
2016-08-30 11:10:04 +0200

    Wine Model + Funny Quote Bot

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,3 +1,16 @@
 class Wine < ApplicationRecord
   has_many :meals
+  before_create :winecolor
+
+
+  def winecolor
+    case self.id_type_vin
+    when 1, 7                       # Rouge
+      self.wine_color = "#5F192C"
+    when 2, 4, 5                    # Blanc
+      self.wine_color = "#C86368"
+    when 3, 6                       # Rosé
+      self.wine_color = "#E4D5A3"
+    end
+  end
 end
